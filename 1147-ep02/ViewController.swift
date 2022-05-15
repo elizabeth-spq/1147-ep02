90a41e44c620d0cb8dad0281c4565255cd1c856b
Elizabeth Silvia Pumacota Quispe <elizabethsilvia.15@gmail.com>
2022-05-15 17:13:01 -0500

    configuracion en xcode

---
+++ 1147-ep02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1147-ep02
+//
+//  Created by Juan Carlos Rodriguez Yarmas on 15/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
